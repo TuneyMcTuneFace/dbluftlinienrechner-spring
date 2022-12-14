02e2c21dc777d6c1255fd4ced0b2d680dd84f505
Jannik B <jannik.e@hotmail.de>
2022-12-14 13:19:09 +0100

    distance control bearbeitet

--- src/main/java/com/bopp/dbrechner/api/v1/distance/DistanceController.java
+++ src/main/java/com/bopp/dbrechner/api/v1/distance/DistanceController.java
@@ -16,35 +16,42 @@ public class DistanceController {
     @GetMapping("/api/v1/distance/{bahnhof1}/{bahnhof2}")
     @JsonAnyGetter
     public Map<String, String> calculateDistance(@PathVariable("bahnhof1") String b1, @PathVariable("bahnhof2") String b2){
-        Map<String, String> respone = new HashMap<String, String>();
+        Map<String, String> response = new HashMap<String, String>();
 
         if(!LuftwegrechnerUtility.ds100HashMap.containsKey(b1)){
-            respone.put("error", "Value 1 nicht da");
-            return respone;
+            response.put("error", "Value 1 nicht da");
+            return response;
         }
 
         if(!LuftwegrechnerUtility.ds100HashMap.containsKey(b2)){
-            respone.put("error", "Value 2 nicht da");
-            return respone;
+            response.put("error", "Value 2 nicht da");
+            return response;
         }
 
         Bahnhof bh1 = LuftwegrechnerUtility.ds100HashMap.get(b1);
         Bahnhof bh2 = LuftwegrechnerUtility.ds100HashMap.get(b2);
 
         if(bh1.getName() == bh2.getName()){
-            respone.put("error", "Selber Bahnhof");
-            return respone;
+            response.put("error", "Selber Bahnhof");
+            return response;
         }
 
         HashMap<String, String> d = LuftwegrechnerUtility.distanceBetweenPoints(bh1, bh2);       
         
-        respone.put("from", bh1.getName());
-        respone.put("to", bh2.getName());
-        respone.put("distance", d.get("distance"));       
-        respone.put("unit", d.get("unit")); 
+        response.put("from", bh1.getName());
+        response.put("to", bh2.getName());
+        response.put("distance", d.get("distance"));       
+        response.put("unit", d.get("unit")); 
 
-        return respone;
+        return response;
     }
 
-    //TODO Vielleicht hier eine API für den wechsel einer CSV Datei machen?
+    @GetMapping("/api/v1/distance/")
+    public Map<String, String> distanceResponse(){
+        Map<String, String> response = new HashMap<String, String>();
+        response.put("error", "Keine Bahnhöfe angegeben");
+        return response;
+    }
+
+    //TODO Vielleicht hier eine API für einen CSV Hot-swap machen? Datei upload?
 }
